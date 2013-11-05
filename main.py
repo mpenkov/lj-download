045e3753bd3acd6c4465d3a355f65cc1e44f43ba
Michael Penkov <misha.penkov@gmail.com>
2013-11-05 23:52:44 +0900

    moving fragile stuff into individual functions

--- main.py
+++ main.py
@@ -29,6 +29,61 @@ def encode_title(title):
     encoded_title = urllib.quote_plus(latin_title.replace(" ", "-"))
     return encoded_title
 
+def parse_previous_link(root):
+    """Parse the link to the chronologically previous blog entry."""
+    prev_entry_url = None
+    links = root.xpath("//span[@class='entry-linkbar-inner']/a/img[@alt='Previous Entry']")
+    if links:
+        prev_entry_url = links[0].getparent().get("href")
+    if DEBUG:
+        print prev_entry_url
+    return prev_entry_url
+
+def parse_title(root):
+    """Parse the title of a LiveJournal entry."""
+    title = None
+    dt = root.xpath("//dt[@class='entry-title']")
+    if dt:
+        title = dt[0].text
+    if DEBUG:
+        print title
+    assert title
+    return title
+
+def parse_timestamp(root):
+    """Parse the timestamp of a LiveJournal entry.
+    Returns a datetime.datetime instance."""
+    updated = None
+    abbr = root.xpath("//abbr[@class='updated']")
+    if abbr:
+        timestamp_str = abbr[0].get("title")
+        # 2013-10-08T11:41:00+03:00
+        # get rid of the UTC offset, since we can't parse it :(
+        # we don't use it anywhere, anyway.
+        timestamp = datetime.datetime.strptime(timestamp_str[:-6], "%Y-%m-%dT%H:%M:%S")
+
+    if DEBUG:
+        print timestamp
+    assert timestamp
+    return timestamp
+
+def parse_entry_text(root):
+    """Parse the actual entry text of a LiveJournal entry.
+    Returns a UTF-8 encoded byte string."""
+    #
+    # Here we only grab the HTML fragment that corresponds to the entry context.
+    # Throw everything else away.
+    #
+    entry_text = None
+    dd = root.xpath("//div[@class='entry-content']")
+    if dd:
+        entry_text = lxml.etree.tostring(dd[0], pretty_print=True, encoding="utf-8")
+
+    if DEBUG:
+        print entry_text
+    assert entry_text
+    return entry_text
+
 class Entry:
     """Represents a single LiveJournal entry.
     Includes functions for downloading an entry from a known URL."""
@@ -59,47 +114,10 @@ class Entry:
         assert r.status_code == 200
 
         root = lxml.html.document_fromstring(r.text)
-
-        prev_entry_url = None
-        links = root.xpath("//span[@class='entry-linkbar-inner']/a/img[@alt='Previous Entry']")
-        if links:
-            prev_entry_url = links[0].getparent().get("href")
-        if DEBUG:
-            print prev_entry_url
-
-        updated = None
-        abbr = root.xpath("//abbr[@class='updated']")
-        if abbr:
-            timestamp_str = abbr[0].get("title")
-            # 2013-10-08T11:41:00+03:00
-            # get rid of the UTC offset, since we can't parse it :(
-            # we don't use it anywhere, anyway.
-            timestamp = datetime.datetime.strptime(timestamp_str[:-6], "%Y-%m-%dT%H:%M:%S")
-            
-        if DEBUG:
-            print timestamp
-        assert timestamp
-
-        title = None
-        dt = root.xpath("//dt[@class='entry-title']")
-        if dt:
-            title = dt[0].text
-        if DEBUG:
-            print title
-        assert title
-
-        #
-        # Here we only grab the HTML fragment that corresponds to the entry context.
-        # Throw everything else away.
-        #
-        entry_text = None
-        dd = root.xpath("//div[@class='entry-content']")
-        if dd:
-            entry_text = lxml.etree.tostring(dd[0], pretty_print=True, encoding="utf-8")
-
-        if DEBUG:
-            print entry_text
-        assert entry_text
+        title = parse_title(root)
+        entry_text = parse_entry_text(root)
+        timestamp = parse_timestamp(root)
+        prev_entry_url = parse_previous_link(root)
 
         return Entry(title, entry_text, timestamp, prev_entry_url)
 
